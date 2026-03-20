a3dc6121569261e8586d743706f34ffb992be513
MD.RAYHAN <md.rayhanx617@gmail.com>
2026-03-20 15:56:53 +0600

    added navigate in home file for routing to breaking news

--- src/Layoutes/HomeLayout/Qzone.jsx
+++ src/Layoutes/HomeLayout/Qzone.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import classImg from '../../../src/assets/class.png'
+import SwimmingImg from '../../../src/assets/swimming.png'
+import playGroundImg from '../../../src/assets/playground.png'
 const Qzone = () => {
     return (
-        <div className='bg-base-200 mt-5 ml-5' >
+        <div className='bg-base-200 mt-5 ml-5 p-5' >
             <h1 className='font-bold text-2xl mb-5' >Qzone</h1>
-            <div>
-                
+            <div className='space-y-5' >
+<img src={classImg} alt="" />
+<img src={SwimmingImg} alt="" />
+<img src={playGroundImg} alt="" />
             </div>
         </div>
     );
