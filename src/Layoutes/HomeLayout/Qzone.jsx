30c93a07962c1cfbe2631763a719b1bc98165140
MD.RAYHAN <md.rayhanx617@gmail.com>
2026-03-20 16:02:34 +0600

    added bg img in qzone

--- src/Layoutes/HomeLayout/Qzone.jsx
+++ src/Layoutes/HomeLayout/Qzone.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import classImg from '../../../src/assets/class.png'
 import SwimmingImg from '../../../src/assets/swimming.png'
 import playGroundImg from '../../../src/assets/playground.png'
+import bgImg from '../../../src/assets/bg.png'
 const Qzone = () => {
     return (
-        <div className='bg-base-200 mt-5 ml-5 p-5' >
+        <div>
+            <div className='bg-base-200 mt-5 ml-5 p-5' >
             <h1 className='font-bold text-2xl mb-5' >Qzone</h1>
             <div className='space-y-5' >
 <img src={classImg} alt="" />
 <img src={SwimmingImg} alt="" />
 <img src={playGroundImg} alt="" />
             </div>
+        </div>
+        <div className='mt-2 ml-5 w-full ' >
+            <img src={bgImg} alt="" />
+        </div>
         </div>
     );
 };
