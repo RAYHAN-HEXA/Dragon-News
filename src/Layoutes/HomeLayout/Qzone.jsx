0cf952877bf6698d0db9d785dbdfecccca2b10e5
MD.RAYHAN <md.rayhanx617@gmail.com>
2026-03-20 15:46:55 +0600

    added 3 pictures in qzone file

--- src/Layoutes/HomeLayout/Qzone.jsx
+++ src/Layoutes/HomeLayout/Qzone.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Qzone = () => {
     return (
-        <div>
-            
+        <div className='bg-base-200 mt-5 ml-5' >
+            <h1 className='font-bold text-2xl mb-5' >Qzone</h1>
+            <div>
+                
+            </div>
         </div>
     );
 };
--- src/Layoutes/HomeLayout/RightAside.jsx
+++ src/Layoutes/HomeLayout/RightAside.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SocialLogin from '../../Components/SocialLOgin/SocialLogin';
 import FindUs from './FindUs';
+import Qzone from './Qzone';
 
 const RightAside = () => {
     return (
         <div>
             <SocialLogin></SocialLogin>
             <FindUs></FindUs>
+            <Qzone></Qzone>
         </div>
     );
 };
